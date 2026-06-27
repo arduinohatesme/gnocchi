b51957b0450a52e39515c725613c5977860c545d
arduinohatesme <awmcmillan128@gmail.com>
2026-06-27 11:51:20 -0500

    Make wheel work (need to find a better way to map keys)

--- shell.nix
+++ shell.nix
@@ -1,12 +1,15 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-26.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
+  kc8_pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
+  }) {};
 in
 
 pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    kicad
-    ergogen
-    openjdk25
+  packages = [
+    kc8_pkgs.kicad
+    pkgs.ergogen
+    pkgs.openjdk25
   ];
 }
